7d963d949c29e9f8f6a85d9aeba6437e60805541
David Etukudo <detukudo1@gmail.com>
2021-05-06 10:55:21 +0100

    secret key restored

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -24,13 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = os.getenv("SECRET_KEY")
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-
+SECRET_KEY = 'django-insecure-%qhhh*3cl=4$8v89n6807$dxxjnjlrmojy24dj+qv#sulfk1vb'
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.getenv("DEBUG")
+# DEBUG = os.getenv("DEBUG")
 
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
